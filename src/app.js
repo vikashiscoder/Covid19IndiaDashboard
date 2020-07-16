a15010a16f27eb2a8b826ba355da026ea8843b47
vikashiscoder <vikashis@yahoo.com>
2020-07-16 16:04:03 +0530

    3 files modified

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
-ReactDOM.render((
-   <Router history = {browserHistory}>
-      <Route path = "/" component = {App}>
-         <IndexRoute component = {App} />
-         <Route path = "home" component = {App} />
-         <Route path = "about" component = {App} />
-         <Route path = "contact" component = {App} />
-      </Route> 
-   </Router>
-), document.getElementById('root'))
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
