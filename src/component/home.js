d8eabec987c1a02e46255caa2ffa645854e011da
vikashiscoder <vikashis@yahoo.com>
2020-07-30 16:55:15 +0530

    1 file modified

--- src/component/home.js
+++ src/component/home.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import styled from 'styled-components';
 
 class Home extends Component {
   componentDidMount(){
-    var map = L.map('mapid', {
-    center: [51.505, -0.09],
-    zoom: 5
-});
+
   }
   render() {
     return (
         <div>
-         <div id="mapid" width="900px" height="900px"></div>
+AAAS
         </div>
     );
   }
