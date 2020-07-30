3bc41cbd32ba8c0dfd6dc1fd1ae565d2f7797408
vikashiscoder <vikashis@yahoo.com>
2020-07-30 16:51:50 +0530

    1 file modified

--- src/component/home.js
+++ src/component/home.js
@@ -12,7 +12,7 @@ class Home extends Component {
   render() {
     return (
         <div>
-         <div id="mapid"></div>
+         <div id="mapid" width="900px" height="900px"></div>
         </div>
     );
   }
