aea2a943a0d01c9ebfbcb9fe16ac05fec12284bb
vikashiscoder <vikashis@yahoo.com>
2020-08-03 11:03:00 +0530

    1 file modified

--- src/component/home.js
+++ src/component/home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Home extends Component {
   componentDidMount(){
-    var mymap = L.map('mapid').setView([51.505, -0.09], 6);
+    var mymap = L.map('mapid').setView([20.5937, 78.9629], 5);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data',
@@ -12,6 +12,12 @@ class Home extends Component {
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoidmlrYXNoaXMiLCJhIjoiY2tkOHI4YnZsMDZpeDJ6bWliM2Y5dTZiaCJ9.EweMjk3iwotgronCLOX-Fw'
 }).addTo(mymap);
+
+//var marker = L.marker([11.1271, 78.6569]).addTo(mymap);
+var popup = L.popup()
+    .setLatLng([11.1271, 78.6569])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
 
   }
   render() {
