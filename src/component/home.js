7f449aa3f9f312f30537cd4e8a6f781ea62a38ec
vikashiscoder <vikashis@yahoo.com>
2020-08-03 11:06:16 +0530

    1 file modified

--- src/component/home.js
+++ src/component/home.js
@@ -19,6 +19,16 @@ var popup = L.popup()
     .setContent("I am a standalone popup.")
     .openOn(mymap);
 
+icon = L.divIcon({
+        className: 'custom-div-icon',
+        html: "<div style='background-color:#c30b82;' class='marker-pin'></div><i class='material-icons'>weekend</i>",
+        iconSize: [30, 42],
+        iconAnchor: [15, 42]
+    });
+    
+L.marker([11.1271, 78.6569], { icon: icon }).addTo(map);
+
+
   }
   render() {
     return (
