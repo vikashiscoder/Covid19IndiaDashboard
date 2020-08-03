5241b58d890001113dc52b27c445c647c892c523
vikashiscoder <vikashis@yahoo.com>
2020-08-03 10:40:29 +0530

    1 file modified

--- src/component/home.js
+++ src/component/home.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 
 class Home extends Component {
   componentDidMount(){
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([51.505, -0.09], 6);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data',
+    
+    id: 'vikashis/ckd8rjoov0rz91io99r2t1bqq',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoidmlrYXNoaXMiLCJhIjoiY2tkOHI4YnZsMDZpeDJ6bWliM2Y5dTZiaCJ9.EweMjk3iwotgronCLOX-Fw'
+}).addTo(mymap);
 
   }
   render() {
