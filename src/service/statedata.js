3c163ad53efb0e09ddead2016f35ee96e2fa3915
vikashiscoder <vikashis@yahoo.com>
2020-08-04 15:12:16 +0530

    1 file modified

--- src/service/statedata.js
+++ src/service/statedata.js
@@ -3,23 +3,52 @@ const statedata = [{name:'Tamil Nadu', lat:11.059821, long:78.387451},
 {name:'Andhra Pradesh', lat:15.9129, long:79.7400},
 {name:'Karnataka', lat:15.317277, long:75.71389},
 {name:'Uttar Pradesh', lat:28.207609, long:79.82666},
-
-{name:'Telangana', lat:17.123184, long:79.208824},
-{name:'Madhya Pradesh', lat:23.473324, long:77.947998},
-{name:'Haryana', lat:29.238478, long:76.431885},
-{name:'Chhattisgarh', lat:21.295132, long:81.828232},
-{name:'Haryana', lat:29.065773, long:76.040497},
-{name:'Madhya Pradesh', lat:25.794033, long:78.116531},
-{name:'Tripura', lat:23.745127, long:91.746826},
+{name:'Delhi', lat:28.7041, long:77.1025},
+{name:'West Bengal', lat:22.978624, long:87.747803},
 {name:'Telangana', lat:17.874857, long:78.100815},
+{name:'Assam', lat:26.244156, long:92.537842},
+{name:'Rajasthan', lat:27.391277, long:73.432617},
+{name:'Bihar', lat:25.0961, long:85.3131},
+{name:'Haryana', lat:29.238478, long:76.431885},
+{name:'Madhya Pradesh', lat:25.794033, long:78.116531},
+{name:'Kerala', lat:10.8505, long:76.2711},
+{name:'Jammu and Kashmir', lat:33.7782, long:76.5762},
+{name:'Punjab', lat:31.1471, long:75.3412},
+{name:'Chhattisgarh', lat:21.295132, long:81.828232},
+
+{name:'Jharkhand', lat:23.6102, long:85.2799},
+{name:'Uttarakhand', lat:30.0668, long:79.0193},
+
+{name:'Goa', lat:20.94092, long:84.803467},
+{name:'Tripura', lat:20.94092, long:84.803467},
+{name:'Puducherry', lat:20.94092, long:84.803467},
+{name:'Manipur', lat:20.94092, long:84.803467},
+{name:'Nagaland', lat:20.94092, long:84.803467},
+{name:'Himachal Pradesh', lat:20.94092, long:84.803467},
+{name:'Arunachal Pradesh', lat:20.94092, long:84.803467},
+{name:'Ladakh', lat:20.94092, long:84.803467},
+{name:'Ladakh', lat:20.94092, long:84.803467},
+{name:'Ladakh', lat:20.94092, long:84.803467},
+{name:'Ladakh', lat:20.94092, long:84.803467},
+{name:'Ladakh', lat:20.94092, long:84.803467},
+{name:'Ladakh', lat:20.94092, long:84.803467},
+{name:'Ladakh', lat:20.94092, long:84.803467},
+
+{name:'Madhya Pradesh', lat:23.473324, long:77.947998},
+
+
+{name:'Haryana', lat:29.065773, long:76.040497},
+,
+{name:'Tripura', lat:23.745127, long:91.746826},
+
 
 {name:'Kerala', lat:10.850516, long:76.27108},
 
-{name:'Assam', lat:26.244156, long:92.537842},
-{name:'West Bengal', lat:22.978624, long:87.747803},
+
+
 {name:'Gujarat', lat:22.309425, long:72.13623},
-{name:'Odisha', lat:20.94092, long:84.803467},
-{name:'Rajasthan', lat:27.391277, long:73.432617},
+
+
 {name:'Himachal Pradesh', lat:32.084206, long:77.571167}];
 
 export default statedata;
